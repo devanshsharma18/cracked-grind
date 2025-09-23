58a569467691e0295847406693c7496f777ce2e8
Devansh Sharma <sdevansh098@gmail.com>
2025-09-23 12:33:09 +0530

    Initial repo structure for Golang grind

---
+++ README.md
@@ -0,0 +1,1 @@
+# Golang Grind 🚀
--- leetcode-neetcode/arrays/twoSum.go
+++ leetcode-neetcode/arrays/twoSum.go
@@ -1,1 +1,30 @@
 package arrays
+
+// brute-force approach:=
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
+// HashMap Approach:=
+func twoSum(nums []int, target int) []int {
+	indices := make(map[int]int)
+
+	for i, n := range nums {
+		indices[n] = i
+	}
+
+	for i, n := range nums {
+		diff := target - n
+		if j, found := indices[diff]; found && j != i {
+			return []int{i, j}
+		}
+	}
+	return []int{}
+}
